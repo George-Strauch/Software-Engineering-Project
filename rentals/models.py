ae09a7024368bfa3f2133d2816e42851ecb894ba
George Strauch <gestrauch5@gmail.com>
2020-04-21 17:19:45 -0500

    update

--- rentals/models.py
+++ rentals/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Property(models.Model):
+    property_description = models.TextField()
+    # posted_by = '' # todo
+    address = models.TextField()
+    price_per_day = models.IntegerField()
+
+
+
+class Reservation(models.Model):
+    # renter = models.ForeignKey()  todo
+    property = models.ForeignKey(Property, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    total_price = models.IntegerField()
+
+
+
+class Feedback(models.Model):
+    rating = models.IntegerField(choices=[(i,i) for i in range(11)])
+    comment = models.TextField()
+    reservation = models.OneToOneField(Reservation, on_delete=models.CASCADE)
+
+
+
+# ---- helper models ----
+# https://stackoverflow.com/questions/34006994/how-to-upload-multiple-images-to-a-blog-post-in-django
+class Image(models.Model):
+    Property = models.ForeignKey(Property, on_delete=models.CASCADE)
+    img = models.ImageField(upload_to=None)
--- rentals/views.py
+++ rentals/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('hello') # todo: this will pass html file for the home page
+    return HttpResponse('hello')  # todo: this will pass html file for the home page
+
+
+def posting_view(request):
+    return HttpResponse('hello')  # todo: this will pass html file for the home page
+
